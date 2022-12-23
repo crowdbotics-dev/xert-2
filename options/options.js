03bb2a9f4fb03bbdf12f40ce055a22567e17bf3b
Crowdbotics <engineering@crowdbotics.com>
2022-12-23 06:22:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xert-2",
+  url: "https://xert-2.botics.co",
+  api: "https://xert-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
